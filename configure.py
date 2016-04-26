1c95aa6d83322eec2cb2876e25eb049e561fd58e
Duarte Nunes <duarte@scylladb.com>
2016-04-26 12:35:08 +0200

    range_tombstone_list: Add unit test
    Signed-off-by: Duarte Nunes <duarte@scylladb.com>

--- configure.py
+++ configure.py
@@ -215,6 +215,7 @@ scylla_tests = [
     'tests/dynamic_bitset_test',
     'tests/auth_test',
     'tests/idl_test',
+    'tests/range_tombstone_list_test',
 ]
 
 apps = [
@@ -574,6 +575,7 @@ tests_not_using_seastar_test_framework = set([
     'tests/managed_vector_test',
     'tests/dynamic_bitset_test',
     'tests/idl_test',
+    'tests/range_tombstone_list_test'
 ])
 
 for t in tests_not_using_seastar_test_framework:
