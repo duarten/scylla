295d7e44ae7445fc146563332b2b672b6f7513f0
Duarte Nunes <duarte@scylladb.com>
2016-05-02 23:31:17 +0200

    range_tombstone_list: Make it ReversiblyMergeable
    This patch implements the ReversiblyMergeable cancellative monoid
    for the range_tombstone_list.
    
    Signed-off-by: Duarte Nunes <duarte@scylladb.com>

--- range_tombstone.hh
+++ range_tombstone.hh
@@ -145,6 +145,10 @@ public:
             container_type::node_algorithms::init(rt._link.this_ptr());
         }
     }
+    struct without_link { };
+    range_tombstone(range_tombstone&& rt, without_link) noexcept
+            : range_tombstone(std::move(rt.start), rt.start_kind, std::move(rt.end), rt.end_kind, std::move(rt.tomb)) {
+    }
     range_tombstone(const range_tombstone& rt)
             : range_tombstone(rt.start, rt.start_kind, rt.end, rt.end_kind, rt.tomb)
     { }
@@ -197,6 +201,11 @@ public:
             ::feed_hash(h, end_kind);
         }
         ::feed_hash(h, tomb);
+    }
+    friend void swap(range_tombstone& rt1, range_tombstone& rt2) {
+        range_tombstone tmp(std::move(rt2), without_link());
+        rt2 = std::move(rt1);
+        rt1 = std::move(tmp);
     }
     friend std::ostream& operator<<(std::ostream& out, const range_tombstone& rt);
     using container_type = bi::set<range_tombstone,
