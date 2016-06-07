d328edc8364b10908b9ae4e7f2bd89f2a046c3fb
Duarte Nunes <duarte@scylladb.com>
2016-06-07 22:24:37 +0200

    thrift: Add lookup_schema function
    Signed-off-by: Duarte Nunes <duarte@scylladb.com>

--- thrift/handler.cc
+++ thrift/handler.cc
@@ -316,8 +316,7 @@ public:
                         [this, thrift_key] (std::pair<std::string, std::vector<Mutation>> cf_mutations) {
                     sstring cf_name = cf_mutations.first;
                     const std::vector<Mutation>& mutations = cf_mutations.second;
-                    auto& cf = lookup_column_family(_db.local(), _ks_name, cf_name);
-                    auto schema = cf.schema();
+                    auto schema = lookup_schema(_db.local(), _ks_name, cf_name);
                     mutation m_to_apply(key_from_thrift(schema, thrift_key), schema);
                     auto empty_clustering_key = clustering_key::make_empty();
                     for (const Mutation& m : mutations) {
@@ -326,7 +325,7 @@ public:
                             if (cosc.__isset.column) {
                                 auto&& col = cosc.column;
                                 bytes cname = to_bytes(col.name);
-                                auto def = cf.schema()->get_column_definition(cname);
+                                auto def = schema->get_column_definition(cname);
                                 if (!def) {
                                     throw make_exception<InvalidRequestException>("column %s not found", col.name);
                                 }
@@ -338,7 +337,7 @@ public:
                                     ttl = std::chrono::duration_cast<gc_clock::duration>(std::chrono::seconds(col.ttl));
                                 }
                                 if (ttl.count() <= 0) {
-                                    ttl = cf.schema()->default_time_to_live();
+                                    ttl = schema->default_time_to_live();
                                 }
                                 ttl_opt maybe_ttl;
                                 if (ttl.count() > 0) {
@@ -667,11 +666,17 @@ private:
         return std::move(def);
     }
     static column_family& lookup_column_family(database& db, const sstring& ks_name, const sstring& cf_name) {
+        if (ks_name.empty()) {
+            throw make_exception<InvalidRequestException>("keyspace not set");
+        }
         try {
             return db.find_column_family(ks_name, cf_name);
-        } catch (std::out_of_range&) {
+        } catch (no_such_column_family&) {
             throw make_exception<InvalidRequestException>("column family %s not found", cf_name);
         }
+    }
+    static schema_ptr lookup_schema(database& db, const sstring& ks_name, const sstring& cf_name) {
+        return lookup_column_family(db, ks_name, cf_name).schema();
     }
     static partition_key key_from_thrift(schema_ptr s, bytes k) {
         if (s->partition_key_size() != 1) {
