7abf3b9eaafe79f27ff6d80695702781dc3a4d80
Duarte Nunes <duarte@scylladb.com>
2016-06-08 17:11:59 +0200

    thrift: Implement truncate verb
    Signed-off-by: Duarte Nunes <duarte@scylladb.com>

--- thrift/handler.cc
+++ thrift/handler.cc
@@ -427,8 +427,13 @@ public:
     }
 
     void truncate(tcxx::function<void()> cob, tcxx::function<void(::apache::thrift::TDelayedException* _throw)> exn_cob, const std::string& cfname) {
-        // FIXME: implement
-        return pass_unimplemented(exn_cob);
+        return with_cob(std::move(cob), std::move(exn_cob), [&] {
+            if (_ks_name.empty()) {
+                throw make_exception<InvalidRequestException>("keyspace not set");
+            }
+
+            return service::get_local_storage_proxy().truncate_blocking(_ks_name, cfname);
+        });
     }
 
     void get_multi_slice(tcxx::function<void(std::vector<ColumnOrSuperColumn>  const& _return)> cob, tcxx::function<void(::apache::thrift::TDelayedException* _throw)> exn_cob, const MultiSliceRequest& request) {
