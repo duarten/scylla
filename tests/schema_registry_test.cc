72c747c9517e0d78312ea37e55bdd63896eaf44c
Duarte Nunes <duarte@scylladb.com>
2017-02-20 20:16:40 +0100

    tests/schema_registry_test: Test async loading with views
    Signed-off-by: Duarte Nunes <duarte@scylladb.com>

--- tests/schema_registry_test.cc
+++ tests/schema_registry_test.cc
@@ -44,6 +44,14 @@ static schema_ptr random_schema() {
            .build();
 }
 
+static schema_ptr random_view(schema_ptr base) {
+    return schema_builder("ks", "cf")
+           .with_column(*base->partition_key_columns().begin())
+           .with_column(base->regular_begin()->name(), bytes_type, column_kind::partition_key)
+           .with_view_info(*base, false, sstring())
+           .build();
+}
+
 SEASTAR_TEST_CASE(test_async_loading) {
     return seastar::async([] {
         storage_service_for_tests ssft;
@@ -67,6 +75,44 @@ SEASTAR_TEST_CASE(test_async_loading) {
         BOOST_REQUIRE(s2_loaded->version() == s2->version());
         auto s2_later = local_schema_registry().get_or_null(s2_loaded->version());
         BOOST_REQUIRE(s2_later);
+    });
+}
+
+SEASTAR_TEST_CASE(test_async_loading_with_views) {
+    return seastar::async([] {
+        storage_service_for_tests ssft;
+        auto s1 = random_schema();
+        auto v1 = random_view(s1);
+        auto s2 = random_schema();
+        auto v2 = random_view(s2);
+
+        auto s1_loaded = local_schema_registry().get_or_load(s1->version(), [s1, v1] (table_schema_version) {
+            return make_ready_future<frozen_schema_and_views>(frozen_schema_and_views(frozen_schema(s1), { frozen_schema(v1) }));
+        }).get0();
+
+        BOOST_REQUIRE(s1_loaded->version() == s1->version());
+        auto s1_later = local_schema_registry().get_or_null(s1->version());
+        BOOST_REQUIRE(s1_later);
+
+        BOOST_REQUIRE_EQUAL(s1_loaded->views().size(), 1);
+        auto v1_from_s1 = s1_loaded->views()[0];
+        BOOST_REQUIRE(v1->version() == v1_from_s1->version());
+        auto v1_from_registry = local_schema_registry().get_or_null(v1->version());
+        BOOST_REQUIRE(v1_from_registry);
+
+        auto s2_loaded = local_schema_registry().get_or_load(s2->version(), [s2, v2] (table_schema_version) {
+            return later().then([s2, v2] { return frozen_schema_and_views(frozen_schema(s2), { frozen_schema(v2) }); });
+        }).get0();
+
+        BOOST_REQUIRE(s2_loaded->version() == s2->version());
+        auto s2_later = local_schema_registry().get_or_null(s2->version());
+        BOOST_REQUIRE(s2_later);
+
+        BOOST_REQUIRE_EQUAL(s2_loaded->views().size(), 1);
+        auto v2_from_s2 = s2_loaded->views()[0];
+        BOOST_REQUIRE(v2->version() == v2_from_s2->version());
+        auto v2_from_registry = local_schema_registry().get_or_null(v2->version());
+        BOOST_REQUIRE(v2_from_registry);
     });
 }
 
