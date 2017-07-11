1d7143cc1db864df284ddaddce72a427deb79cc3
Duarte Nunes <duarte@scylladb.com>
2017-07-11 12:23:59 +0200

    sstables: Release the flush permit before fsyncing
    This allows a queued flush to start while we fsync the current
    sstable, which helps reduce the overall time new writes are blocked on
    dirty memory.
    
    Signed-off-by: Duarte Nunes <duarte@scylladb.com>

--- memtable-sstable.hh
+++ memtable-sstable.hh
@@ -25,6 +25,7 @@
 
 #pragma once
 
+#include "dirty_memory_manager.hh"
 #include "memtable.hh"
 #include "sstables/sstables.hh"
 #include <seastar/core/future.hh>
@@ -33,7 +34,11 @@
 future<>
 write_memtable_to_sstable(memtable& mt,
         sstables::shared_sstable sst,
+        sstable_write_permit&& permit,
         bool backup = false,
         const io_priority_class& pc = default_priority_class(),
         bool leave_unsealed = false);
 
+future<>
+write_memtable_to_sstable(memtable& mt,
+        sstables::shared_sstable sst);
