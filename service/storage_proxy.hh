d687e7bef6b7f910b3c4abeb8abef6ff084555e8
Duarte Nunes <duarte@scylladb.com>
2018-10-15 21:12:51 +0100

    service/storage_proxy: Prepare to receive replica view update backlog
    In subsequent patches, replicas will reply to the coordinator with
    their view update backlog. Before introducing changes to the
    messaging_service, prepare the storage_proxy to receive and store
    those backlogs.
    
    Signed-off-by: Duarte Nunes <duarte@scylladb.com>

--- service/storage_proxy.hh
+++ service/storage_proxy.hh
@@ -50,6 +50,7 @@
 #include "db/read_repair_decision.hh"
 #include "db/write_type.hh"
 #include "db/hints/manager.hh"
+#include "db/view/view_update_backlog.hh"
 #include "utils/histogram.hh"
 #include "utils/estimated_histogram.hh"
 #include "tracing/trace_state.hh"
@@ -71,6 +72,11 @@ class abstract_read_executor;
 class mutation_holder;
 
 using replicas_per_token_range = std::unordered_map<dht::token_range, std::vector<utils::UUID>>;
+
+struct view_update_backlog_timestamped {
+    db::view::update_backlog backlog;
+    api::timestamp_type ts;
+};
 
 class storage_proxy : public seastar::async_sharded_service<storage_proxy> /*implements StorageProxyMBean*/ {
 public:
@@ -171,6 +177,8 @@ private:
             clock_type::time_point,
             tracing::trace_state_ptr,
             bool> _mutate_stage;
+    std::unordered_map<gms::inet_address, view_update_backlog_timestamped> _view_update_backlogs;
+
 private:
     void uninit_messaging_service();
     future<coordinator_query_result> query_singular(lw_shared_ptr<query::read_command> cmd,
@@ -179,8 +187,8 @@ private:
             coordinator_query_options optional_params);
     response_id_type register_response_handler(shared_ptr<abstract_write_response_handler>&& h);
     void remove_response_handler(response_id_type id);
-    void got_response(response_id_type id, gms::inet_address from);
-    void got_failure_response(response_id_type id, gms::inet_address from, size_t count);
+    void got_response(response_id_type id, gms::inet_address from, stdx::optional<db::view::update_backlog> backlog);
+    void got_failure_response(response_id_type id, gms::inet_address from, size_t count, stdx::optional<db::view::update_backlog> backlog);
     future<> response_wait(response_id_type id, clock_type::time_point timeout);
     ::shared_ptr<abstract_write_response_handler>& get_write_response_handler(storage_proxy::response_id_type id);
     response_id_type create_write_response_handler(keyspace& ks, db::consistency_level cl, db::write_type type, std::unique_ptr<mutation_holder> m, std::unordered_set<gms::inet_address> targets,
@@ -271,6 +279,8 @@ private:
             write_stats& stats);
 
     db::view::update_backlog get_local_view_update_backlog() const;
+
+    void maybe_update_view_backlog_of(gms::inet_address, stdx::optional<db::view::update_backlog>);
 public:
     storage_proxy(distributed<database>& db, config cfg);
     ~storage_proxy();
