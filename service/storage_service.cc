aa6df0d01efc39a8ee3c77655b9794852e50d22e
Duarte Nunes <duarte@scylladb.com>
2018-10-18 10:52:38 +0100

    service: Advertise view update backlog over gossip
    This lays the groundwork for brokering a node's view update
    backlog across the whole cluster. This is needed for when a
    coordinator does not contact a given replica for a long time, and uses
    a backlog view that is outdated and causes requests to be
    unnecessarily delayed.
    
    Signed-off-by: Duarte Nunes <duarte@scylladb.com>

--- service/storage_service.cc
+++ service/storage_service.cc
@@ -393,6 +393,7 @@ void storage_service::prepare_to_join(std::vector<inet_address> loaded_endpoints
     app_states.emplace(gms::application_state::SUPPORTED_FEATURES, value_factory.supported_features(features));
     app_states.emplace(gms::application_state::CACHE_HITRATES, value_factory.cache_hitrates(""));
     app_states.emplace(gms::application_state::SCHEMA_TABLES_VERSION, versioned_value(db::schema_tables::version));
+    app_states.emplace(gms::application_state::VIEW_BACKLOG, versioned_value(""));
     slogger.info("Starting up server gossip");
 
     auto& gossiper = gms::get_local_gossiper();
